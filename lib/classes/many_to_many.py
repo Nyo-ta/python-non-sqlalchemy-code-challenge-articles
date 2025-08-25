9687ed4009faba9c4bd1640f9921a4eaa711159a
samuel nyota karanja <samuel.karanja3@student.moringaschool.com>
2025-08-25 14:38:40 +0300

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,53 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+        Article.all.append(self)
+
+
 class Author:
     def __init__(self, name):
         self.name = name
 
     def articles(self):
-        pass
+        return [a for a in Article.all if a.author == self]
 
     def magazines(self):
-        pass
+        return list({a.magazine for a in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        mags = self.magazines()
+        if not mags:
+            return None
+        return list({m.category for m in mags})
+
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        Magazine.all.append(self)
 
     def articles(self):
-        pass
+        return [a for a in Article.all if a.magazine == self]
 
     def contributors(self):
-        pass
+        return list({a.author for a in self.articles()})
 
     def article_titles(self):
-        pass
+        if not self.articles():
+            return None
+        return [a.title for a in self.articles()]
 
     def contributing_authors(self):
-        pass
+        authors = [a.author for a in self.articles()]
+        result = [auth for auth in set(authors) if authors.count(auth) > 2]
+        return result if result else None
