2eddaf9a6f2a6c5f909f9e15a7a4d8fc3324526a
samuel nyota karanja <samuel.karanja3@student.moringaschool.com>
2025-08-28 16:19:07 +0300

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,31 +1,32 @@
-class Article:
-    all = []
-
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        Article.all.append(self)
-
-
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise Exception("Invalid author name")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # ignore attempts to change after init
+        if not hasattr(self, "_name"):
+            if isinstance(value, str) and len(value) > 0:
+                self._name = value
 
     def articles(self):
-        return [a for a in Article.all if a.author == self]
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        return list({a.magazine for a in self.articles()})
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
         return Article(self, magazine, title)
 
     def topic_areas(self):
         mags = self.magazines()
-        if not mags:
-            return None
-        return list({m.category for m in mags})
+        return list({m.category for m in mags}) if mags else None
 
 
 class Magazine:
@@ -36,18 +37,69 @@ class Magazine:
         self.category = category
         Magazine.all.append(self)
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 2 <= len(new_name) <= 16:
+            self._name = new_name
+        # else ignore invalid values
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if isinstance(new_category, str) and len(new_category) > 0:
+            self._category = new_category
+        # else ignore invalid values
+
     def articles(self):
-        return [a for a in Article.all if a.magazine == self]
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        return list({a.author for a in self.articles()})
+        return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        if not self.articles():
-            return None
-        return [a.title for a in self.articles()]
+        arts = self.articles()
+        return [a.title for a in arts] if arts else None
 
     def contributing_authors(self):
         authors = [a.author for a in self.articles()]
-        result = [auth for auth in set(authors) if authors.count(auth) > 2]
-        return result if result else None
+        return [auth for auth in set(authors) if authors.count(auth) > 2] or None
+
+    @classmethod
+    def top_publisher(cls):
+        if not Article.all:
+            return None
+        return max(cls.all, key=lambda m: len(m.articles()))
+
+
+class Article:
+    all = []
+
+    def __init__(self, author, magazine, title):
+        if not isinstance(author, Author) or not isinstance(magazine, Magazine):
+            raise Exception("Invalid Author or Magazine")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise Exception("Invalid Title")
+        self._title = title
+        self.author = author
+        self.magazine = magazine
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # ignore attempts to change after init
+        if not hasattr(self, "_title"):
+            if isinstance(value, str) and 5 <= len(value) <= 50:
+                self._title = value
+ 
+
